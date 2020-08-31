edcdb9e7375ee6571d83ea995b1ccccdc4c145d5
Vishula <vishula.gamaetige@my.jcu.edu.au>
2020-08-31 20:33:16 +1000

    Add files via upload
    Python Crash Course Chapter 5: IF statements. 
    Disclaimer: The repository isn't organized properly. Sorry for the inconvenience. 
    5-1 to 5-8 Conditional Tests
    ifstatement.py: Practice 
    usingifstatementswithlists.py: More Practice 
    usingifstatementswithlists.py: Using If loops on Lists

---
+++ 5-3.py
@@ -0,0 +1,59 @@
+# 5-3 and 5-4 Alien colors #1 & #2
+
+# alien_color = ['green', 'yellow', 'red']
+# alien = 'red'
+# if alien == 'green':
+#     points = 5
+# else:
+#     points = 10
+#
+#
+# print(f"Player earned {points} points.")
+
+# 5-5 Alien colors #3
+
+# alien_color = ['green', 'yellow', 'red']
+# alien = 'yellow'
+# if alien == 'green':
+#     point = 5
+# elif alien == 'yellow':
+#     point = 10
+# elif alien == 'red':
+#     point = 15
+#
+# print(f"Player earned {point} points. ")
+
+# 5-6 Stages of life
+# age = 1201
+#
+# if age < 2:
+#     stage_of_life = 'baby'
+# elif 2 <= age < 4:
+#     stage_of_life = 'toddler'
+# elif 4 <= age < 13:
+#     stage_of_life = 'kid'
+# elif 13 <= age < 20:
+#     stage_of_life = 'teenager'
+# elif 20 <= age < 65:
+#     stage_of_life = 'adult'
+# elif 65 <= age <120:
+#     stage_of_life = "elder"
+# elif 120 <= age:
+#     stage_of_life = 'Afterlife'
+#
+# print(f"Stage of life is a {stage_of_life}")
+
+# 5-7 Favourite fruit
+
+# fav_fruits = ['mango', 'blueberry', 'apple', 'grapes']
+# fav = 'apple'
+# fav2 = 'mango'
+# fav3 = 'juice'
+#
+# if fav in fav_fruits:
+#     print(f"I really like {fav}")
+# if fav2 in fav_fruits:
+#     print(f"I really like {fav2}")
+# if fav3 in fav_fruits:
+#     print(f"I really like {fav3}")
+
