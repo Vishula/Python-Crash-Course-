f9bbc7b6d734d25a8ca796574ec5bdefd72d32d9
Admin <vishula.gamaetige@my.jcu.edu.au>
2020-09-14 23:19:08 +1000

    6-9 Favourite Place
    page 112
    Book: Python Crash Course

---
+++ numb.py
@@ -0,0 +1,6 @@
+cities = {'tsv':{'name':'townsville', 'rank':22},
+          'bne':{'name':'brisbane', 'rank':12},
+          'mel':{'name':'melbourne', 'rank': 1}}
+
+for city in cities:
+    print(city)
