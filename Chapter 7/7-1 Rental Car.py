4e2ed8779308eae3e4397ca7e7a8304111191c7d
Vishula <vishula.gamaetige@my.jcu.edu.au>
2020-09-19 22:38:44 +1000

    Chapter 7: Exercise 7-1. Rental Car
    Page 117
    Book: Python Crash Course

---
+++ Chapter 7/7-1 Rental Car.py
@@ -0,0 +1,8 @@
+# Program that asks for a  what type of car user like to rent and print it out
+# with a message
+
+# input() function to get car name
+car = input("What kind of a rental car would you like: ")
+
+#f-string format to print user input
+print(f'Let me see if i can find you a {car.title()}')
